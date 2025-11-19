77428922a2026a268e3922474ee9da899bea3843
mousethain <mousethain@gmail.com>
2025-11-19 12:19:53 +0700

    Update import from cybervpn to mousevpn modules

--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
 from cybervpn import *
 from importlib import import_module
-from cybervpn.modules import ALL_MODULES
+from mousevpn.modules import ALL_MODULES
 for module_name in ALL_MODULES:
-        imported_module = import_module("cybervpn.modules." + module_name)
+        imported_module = import_module("mousevpn.modules." + module_name)
 bot.run_until_disconnected()
 
