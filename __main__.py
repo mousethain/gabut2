619bb1f93ef9311d97e6985377f3dc3697cd27ad
mousethain <mousethain@gmail.com>
2025-12-02 05:08:44 +0700

    Delete __main__.py

--- __main__.py
+++
@@ -1,7 +0,0 @@
-from mousevpn import *
-from importlib import import_module
-from mousevpn.modules import ALL_MODULES
-for module_name in ALL_MODULES:
-        imported_module = import_module("mousevpn.modules." + module_name)
-bot.run_until_disconnected()
-
